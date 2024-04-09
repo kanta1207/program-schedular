25e66f26cbdee18463849eb3cad40322193c2b96
kanta1207 <kan0712livepool@gmail.com>
2024-04-08 23:57:16 -0700

    fix: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://program-schedular.vercel.app',
+    ],
+  });
   await app.listen(process.env.APP_PORT); // TODO: Replace port number with env variable
 }
 bootstrap();
